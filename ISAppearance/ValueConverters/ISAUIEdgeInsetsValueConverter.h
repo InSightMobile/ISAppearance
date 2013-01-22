990b04d40a24b6069ee3588eaabba6a200b56edf
yarry <yarryp@gmail.com>
2013-01-22 04:55:59 +0400

    UIEdgeInsets support

---
+++ ISAppearance/ValueConverters/ISAUIEdgeInsetsValueConverter.h
@@ -0,0 +1,11 @@
+//
+// 
+
+
+
+#import <Foundation/Foundation.h>
+#import "ISAValueConverter.h"
+
+
+@interface ISAUIEdgeInsetsValueConverter : ISAValueConverter
+@end
---
+++ ISAppearance/ValueConverters/ISAUIEdgeInsetsValueConverter.m
@@ -0,0 +1,30 @@
+//
+// 
+
+
+
+#import "ISAUIEdgeInsetsValueConverter.h"
+
+
+@implementation ISAUIEdgeInsetsValueConverter
+{
+
+}
+
+- (id)createFromNode:(id)node
+{
+    UIEdgeInsets insets = UIEdgeInsetsZero;
+
+    if ([node isKindOfClass:[NSArray class]]) {
+
+        if ([node count] == 4) {
+            insets =  UIEdgeInsetsMake([[node objectAtIndex:0] floatValue],
+                                   [[node objectAtIndex:1] floatValue],
+                                    [[node objectAtIndex:2] floatValue],
+                                   [[node objectAtIndex:3] floatValue]);
+        }
+    }
+    return [NSValue value:&insets withObjCType:@encode(UIEdgeInsets)];
+}
+
+@end
