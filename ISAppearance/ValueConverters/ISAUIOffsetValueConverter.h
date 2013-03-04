4cae85da59ddc5819065be173f064eb02e9990a1
yarry <yarryp@gmail.com>
2013-03-05 02:21:05 +0400

    UIOffset support

---
+++ ISAppearance/ValueConverters/ISAUIOffsetValueConverter.h
@@ -0,0 +1,13 @@
+//
+//  ISAUIOffsetValueConverter.h
+//  socials
+//
+//  Created by yar on 05.03.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import "ISAValueConverter.h"
+
+@interface ISAUIOffsetValueConverter : ISAValueConverter
+
+@end
---
+++ ISAppearance/ValueConverters/ISAUIOffsetValueConverter.m
@@ -0,0 +1,41 @@
+//
+//  ISAUIOffsetValueConverter.m
+//  socials
+//
+//  Created by yar on 05.03.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import "ISAUIOffsetValueConverter.h"
+
+@implementation ISAUIOffsetValueConverter
+
+- (id)createFromNode:(id)node
+{
+    UIOffset insets = UIOffsetZero;
+    
+    if ([node isKindOfClass:[NSArray class]]) {
+        
+        if ([node count] == 2) {
+            insets =  UIOffsetMake([[node objectAtIndex:0] floatValue],
+                                       [[node objectAtIndex:1] floatValue]);
+        }
+        else if([node count] == 1) {
+            insets =  UIOffsetMake(0,
+                                   [[node objectAtIndex:0] floatValue]);
+        }
+    }
+    else if([node isKindOfClass:[NSNumber class]]) {
+        insets =  UIOffsetMake(0,
+                               [node floatValue]);
+    }
+    else if([node isKindOfClass:[NSString class]]) {
+        insets =  UIOffsetMake(0,
+                               [node floatValue]);
+    }
+    
+    return [NSValue value:&insets withObjCType:@encode(UIOffset)];
+}
+
+
+@end
