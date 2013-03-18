bf187be0e480e815c5a336cf6ddccc07ab9cc0d4
yarry <yarryp@gmail.com>
2013-03-19 02:12:18 +0400

    CGSize support

---
+++ ISAppearance/ValueConverters/ISACGSizeValueConverter.h
@@ -0,0 +1,13 @@
+//
+//  ISACGSizeValueConverter.h
+//  socials
+//
+//  Created by yar on 18.03.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import "ISAValueConverter.h"
+
+@interface ISACGSizeValueConverter : ISAValueConverter
+
+@end
---
+++ ISAppearance/ValueConverters/ISACGSizeValueConverter.m
@@ -0,0 +1,40 @@
+//
+//  ISACGSizeValueConverter.m
+//  socials
+//
+//  Created by yar on 18.03.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import "ISACGSizeValueConverter.h"
+
+@implementation ISACGSizeValueConverter
+
+- (id)createFromNode:(id)node
+{
+    CGSize size = CGSizeZero;
+    
+    if ([node isKindOfClass:[NSArray class]]) {
+        
+        if ([node count] == 2) {
+            size =  CGSizeMake([[node objectAtIndex:0] floatValue],
+                                   [[node objectAtIndex:1] floatValue]);
+        }
+        else if([node count] == 1) {
+            size =  CGSizeMake([[node objectAtIndex:0] floatValue],
+                                   [[node objectAtIndex:0] floatValue]);
+        }
+    }
+    else if([node isKindOfClass:[NSNumber class]]) {
+        size =  CGSizeMake([node floatValue],
+                               [node floatValue]);
+    }
+    else if([node isKindOfClass:[NSString class]]) {
+        size =  CGSizeMake([node floatValue],
+                               [node floatValue]);
+    }
+    
+    return [NSValue value:&size withObjCType:@encode(CGSize)];
+}
+
+@end
