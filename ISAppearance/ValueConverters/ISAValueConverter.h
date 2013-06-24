7fe771c5ac26109f33b06cd98697cddf7e3972bd
yarry <yarryp@gmail.com>
2013-06-24 11:36:44 +0400

    fix linking problems

--- ISAppearance/ValueConverters/ISAValueConverter.h
+++ ISAppearance/ValueConverters/ISAValueConverter.h
@@ -1,7 +1,6 @@
 //
 // 
 
-
 #import <Foundation/Foundation.h>
 #import "YKTag.h"
 
--- YAML/YamlKit/NSData+Base64.h
+++ YAML/YamlKit/NSData+Base64.h
@@ -14,20 +14,23 @@
 
 #import <Foundation/Foundation.h>
 
-void *NewBase64Decode(
+
+void *ISA_NewBase64Decode(
 	const char *inputBuffer,
 	size_t length,
 	size_t *outputLength);
 
-char *NewBase64Encode(
+char *ISA_NewBase64Encode(
 	const void *inputBuffer,
 	size_t length,
 	bool separateLines,
 	size_t *outputLength);
 
-@interface NSData (Base64)
 
-+ (NSData *)dataFromBase64String:(NSString *)aString;
-- (NSString *)base64EncodedString;
+@interface NSData (ISA_Base64)
+
++ (NSData *)ISA_dataFromBase64String:(NSString *)aString;
+- (NSString *)ISA_base64EncodedString;
 
 @end
+
