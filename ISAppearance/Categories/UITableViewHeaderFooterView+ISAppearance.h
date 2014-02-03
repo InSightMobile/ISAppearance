ea0cfd88400ef669e119e1c4df3403d64ae0e229
yarry <yarryp@gmail.com>
2014-02-03 19:15:21 +0400

    support UITableViewHeaderFooterView

---
+++ ISAppearance/Categories/UITableViewHeaderFooterView+ISAppearance.h
@@ -0,0 +1,9 @@
+//
+// Created by Ярослав on 03.02.14.
+// Copyright (c) 2014 yarryp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UITableViewHeaderFooterView (ISAppearance)
+@end
---
+++ ISAppearance/Categories/UITableViewHeaderFooterView+ISAppearance.m
@@ -0,0 +1,32 @@
+//
+// Created by Ярослав on 03.02.14.
+// Copyright (c) 2014 yarryp. All rights reserved.
+//
+
+#import "UITableViewHeaderFooterView+ISAppearance.h"
+#import "UIImage+ISAColor.h"
+
+
+@implementation UITableViewHeaderFooterView (ISAppearance)
+
+- (void)setBackgroundViewImage:(UIImage *)image
+{
+    if ([self.backgroundView isKindOfClass:[UIImageView class]]) {
+        [(UIImageView *) self.backgroundView setImage:image];
+    }
+    else {
+        UIImageView *view = [[UIImageView alloc] initWithImage:image];
+        view.frame = self.bounds;
+        view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        [self setBackgroundView:view];
+    }
+}
+
+- (void)setBackgroundViewColor:(UIColor *)color
+{
+    [self setBackgroundViewImage:[UIImage imageWithColor:color]];
+}
+
+
+
+@end
