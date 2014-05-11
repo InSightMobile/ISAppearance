365dde17b08f2a40e3e951f77d9da0ad53894c49
Ярослав Пономаренко <yarryp@gmail.com>
2014-05-11 08:46:07 -0700

    support bar position

---
+++ ISAppearance/ValueConverters/ISAUIBarPositionValueConverter.h
@@ -0,0 +1,13 @@
+//
+// 
+
+
+
+#import <Foundation/Foundation.h>
+#import "ISAValueConverter.h"
+#import "ISAEnumValueConverter.h"
+
+
+@interface ISAUIBarPositionValueConverter : ISAEnumValueConverter
+
+@end
---
+++ ISAppearance/ValueConverters/ISAUIBarPositionValueConverter.m
@@ -0,0 +1,29 @@
+//
+// 
+
+#import "ISAUIBarPositionValueConverter.h"
+
+@implementation ISAUIBarPositionValueConverter
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.mapping = @{
+
+                @"UIBarPositionAny" : @(UIBarPositionAny),
+                @"UIBarPositionBottom" : @(UIBarPositionBottom),
+                @"UIBarPositionTop" : @(UIBarPositionTop),
+                @"UIBarPositionTopAttached" : @(UIBarPositionTopAttached),
+
+                @"any" : @(UIBarPositionAny),
+                @"bottom" : @(UIBarPositionBottom),
+                @"top" : @(UIBarPositionTop),
+                @"topAttached" : @(UIBarPositionTopAttached),
+        };
+    }
+    return self;
+}
+
+
+@end
