29c240d79482f217e281cdf8144311e7b5d52c22
Ярослав Пономаренко <yarryp@gmail.com>
2014-07-18 10:40:42 +0400

    declare UITableViewCell category methods in header

--- ISAppearance/Categories/UITableViewCell+ISAppearance.h
+++ ISAppearance/Categories/UITableViewCell+ISAppearance.h
@@ -6,4 +6,13 @@
 #import <Foundation/Foundation.h>
 
 @interface UITableViewCell (ISAppearance)
+
+- (void)setBackgroundViewImage:(UIImage *)image;
+
+- (void)setSelectedBackgroundViewImage:(UIImage *)image;
+
+- (void)setBackgroundViewColor:(UIColor *)color;
+
+- (void)setSelectedBackgroundViewColor:(UIColor *)color;
+
 @end
