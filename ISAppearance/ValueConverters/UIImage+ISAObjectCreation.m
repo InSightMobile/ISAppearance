cfe9ad5e33e46509e43f1042b2239072c0c07618
yarry <yarryp@gmail.com>
2014-12-10 05:21:59 +0300

    reformat code

--- ISAppearance/ValueConverters/UIImage+ISAObjectCreation.m
+++ ISAppearance/ValueConverters/UIImage+ISAObjectCreation.m
@@ -39,24 +39,28 @@
             image = [ISACode codeWithClass:[UIImage class] format:@"[UIImage imageWithColor:%@]", firstCodeParam];
         }
         else if (firstCodeParam.codeClass == [NSString class]) {
-            image = [ISACode codeWithClass:[UIImage class] flags:ISACodeCached format:@"[UIImage imageNamed:%@]", firstCodeParam];
+            image =
+                    [ISACode codeWithClass:[UIImage class] flags:ISACodeCached format:@"[UIImage imageNamed:%@]", firstCodeParam];
         }
 
         if ([node count] == 2) {
 
             ISACode *secondCodeParam = [ISACode codeForObject:[node objectAtIndex:1]];
 
-            if(secondCodeParam.codeClass == [NSString class]) {
+            if (secondCodeParam.codeClass == [NSString class]) {
                 NSString *modeName = secondCodeParam.sourceString;
-                if([modeName isEqualToString:@"original"]) {
-                    image = [ISACode codeWithClass:[UIImage class] format:@"[%@ imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]]", image];
+                if ([modeName isEqualToString:@"original"]) {
+                    image =
+                            [ISACode codeWithClass:[UIImage class] format:@"[%@ imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]]", image];
                 }
-                else if([modeName isEqualToString:@"template"]) {
-                    image = [ISACode codeWithClass:[UIImage class] format:@"[%@ imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]]", image];
+                else if ([modeName isEqualToString:@"template"]) {
+                    image =
+                            [ISACode codeWithClass:[UIImage class] format:@"[%@ imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]]", image];
                 }
             }
             else {
-                image = [ISACode codeWithClass:[UIImage class] format:@"[%@ resizableImageWithCapInsets:%@]", image, secondCodeParam];
+                image =
+                        [ISACode codeWithClass:[UIImage class] format:@"[%@ resizableImageWithCapInsets:%@]", image, secondCodeParam];
             }
         }
         else if ([node count] == 3) {
@@ -109,7 +113,6 @@
         if ([node count] == 0) {
             return [UIImage new];
         }
-
         id firstParam = [node objectAtIndex:0];
 
         if ([firstParam isKindOfClass:[UIColor class]]) {
@@ -120,39 +123,32 @@
         }
 
         if ([node count] == 2) {
-
             id mode = [node objectAtIndex:1];
             if ([mode isKindOfClass:[NSString class]]) {
                 NSString *modeName = mode;
-                if([modeName isEqualToString:@"original"]) {
+                if ([modeName isEqualToString:@"original"]) {
                     image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
                 }
-                else if([modeName isEqualToString:@"template"]) {
+                else if ([modeName isEqualToString:@"template"]) {
                     image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
                 }
             }
             if ([mode isKindOfClass:[NSValue class]]) {
-
                 UIEdgeInsets insets;
                 [mode getValue:&insets];
                 image = [image resizableImageWithCapInsets:insets];
             }
         }
         else if ([node count] == 3) {
-
-            image =
-                    [image stretchableImageWithLeftCapWidth:[[node objectAtIndex:1] intValue]
+            image = [image stretchableImageWithLeftCapWidth:[[node objectAtIndex:1] intValue]
                                                topCapHeight:[[node objectAtIndex:2] intValue]];
-
         }
         else if ([node count] == 5) {
-
-            UIEdgeInsets insets =
-                    UIEdgeInsetsMake(
-                            [[node objectAtIndex:1] intValue],
-                            [[node objectAtIndex:2] intValue],
-                            [[node objectAtIndex:3] intValue],
-                            [[node objectAtIndex:4] intValue]);
+            UIEdgeInsets insets = UIEdgeInsetsMake(
+                    [[node objectAtIndex:1] intValue],
+                    [[node objectAtIndex:2] intValue],
+                    [[node objectAtIndex:3] intValue],
+                    [[node objectAtIndex:4] intValue]);
 
             image = [image resizableImageWithCapInsets:insets];
         }
